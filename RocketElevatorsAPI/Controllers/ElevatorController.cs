39e2ba1f72e46eedf029c7aeb3cc0cfd676e1e82
kngBMermaid <louis.marmen.92@gmail.com>
2020-12-03 16:44:37 -0500

    PreDeployv13

--- RocketElevatorsAPI/Controllers/ElevatorController.cs
+++ RocketElevatorsAPI/Controllers/ElevatorController.cs
@@ -115,7 +115,7 @@ namespace RocketElevatorsAPI.Controllers
             List<Elevator> columnsElevators = new List<Elevator>();
             foreach(Elevator elevator in elevatorsAll)
             {
-                if (elevator.Column_Id == id)
+                if ((elevator.Column_Id) == id.ToLong())
                 {
                     columnsElevators.Add(elevator);
                 }
